14f4a8d5b7c8ce26b2e5cd63257d59fe9c5e909f
knaopel <kurt@kurtopel.com>
2015-03-24 13:57:08 -0400

    Export First Attempt

---
+++ Migrate-SPTaxononmy.ps1
@@ -0,0 +1,5 @@
+param(
+$TargetUrl
+,$SourceUrl
+)
+
---
+++ SPTaxonomyImportExport.ps1
@@ -0,0 +1,65 @@
+﻿function get-SPTagsFromPage {
+	param(
+		[Parameter(Mandatory = $true)]
+		[Microsoft.SharePoint.SPListItem]$PageItem
+	)
+#	$tags = New-Object -TypeName Microsoft.SharePoint.Taxonomy.TaxonomyFieldValueCollection -ArgumentList $PageItem["Tags"]
+	
+#	$tags | % {
+#	Write-Host "`t$($_)"
+#	}
+	
+	
+	return $PageItem["Tags"]
+}
+
+function Get-SPPagesFromWeb {
+	param(
+		[Parameter(Mandatory = $true)]
+		[Microsoft.SharePoint.SPWeb]$Web
+	)
+	$pages = @()
+	
+	$pl = $Web.Lists.TryGetList("Pages")
+	if($pl) {
+		$pl.Items | % {
+			Write-Host "$($_.Name): $($_['Tags'])"
+			$myPage = $_ | Select Id, Title, Url, Name
+			$myPage | Add-Member -MemberType NoteProperty -Name "Tags" -Value (Get-SPTagsFromPage -PageItem $_)
+#			$myPage | Add-Member -MemberType NoteProperty -Name "Tags" -Value $_["Tags"]
+			$pages += $myPage
+		}
+	} else {
+		$pages = $null
+	}
+	
+	return $pages
+}
+
+
+function Export-SPTaxonomy {
+	param(
+    [Parameter(Mandatory = $true)]
+    [String]$SiteUrl,
+    [Parameter(Mandatory = $true)]
+    [String]$OutputXmlPath
+    )
+	
+	$webs = @()
+	$web = Get-SPWeb -Identity $SiteUrl
+	
+	$myWeb = $web | Select Id, Title, ServerRelativeUrl
+	$myWeb | Add-Member -MemberType NoteProperty -Name "Pages" -Value (Get-SPPagesFromWeb -Web $web)
+	
+	$webs += $myWeb
+	
+	$web.Webs | % {
+		$myWeb = $_ | Select Id, Title, ServerRelativeUrl
+		$myWeb | Add-Member -MemberType NoteProperty -Name "Pages" -Value (Get-SPPagesFromWeb -Web $_)
+		$webs += $myWeb
+	}
+	
+	Export-Clixml -Depth 9 -InputObject $webs -Path $OutputXmlPath
+}
+
+Export-SPTaxonomy -SiteUrl http://qa.oakgov.com/health -OutputXmlPath D:\health-tags.xml
